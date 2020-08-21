3e220d35edbacb972dc43d41edff056ae0389994
Sanny Do <sanny.do94@gmail.com>
2020-08-21 19:45:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,6 +12,6 @@ while row_index < src.count do
   
   row_index += 1
 end
-just_the_strings.join= ("The" "quick" "brown" "fox" "studied" "Ruby")
+just_the_strings.join= ("")
 end
 join_nested_strings
