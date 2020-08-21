1b51b4a8e4a2652d69ea56283c8a042322a61061
Sanny Do <sanny.do94@gmail.com>
2020-08-21 19:48:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
-just_the_strings == []
+just_the_strings = []
  row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
-      just_the_strings  << src[row_index][element_index]
+      just_the_strings.join  << src[row_index][element_index]
     end
     element_index += 1
   end
